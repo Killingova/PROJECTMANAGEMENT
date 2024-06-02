40e63a63283b6ed94c679b2160dceb9b0783701a
Killingova <ms.zhivkova@googlemail.com>
2024-06-02 18:19:06 +0200

    02..0.06.24

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,35 @@
+import { useState } from "react";
 import NewProject from "./components/NewProject.jsx";
+import NoProjectSelected from "./components/NoProjectSelected.jsx";
 import ProjectSidebar from "./components/ProjectSidebar.jsx";
 
 function App() {
+  const [projectsState, setProjectsState] = useState({
+    selectedProjectID: undefined,
+    projects: []
+  });
+
+  function handleStartAddProject() {
+    setProjectsState(prevState => {
+      return {
+        ...prevState,
+        selectedProjectID: null,
+      };
+    });
+  }
+
+  let content;
+
+  if (projectsState.selectedProjectID === null) {
+    content = <NewProject />;
+  } else if (projectsState.selectedProjectID === undefined) {
+    content = <NoProjectSelected onStartAddProject={handleStartAddProject} />;
+  }
+
   return (
     <main className="h-screen my-8 flex gap-8">
-      <ProjectSidebar/>
-      <NewProject/>
+      <ProjectSidebar onStartAddProject={handleStartAddProject} />
+      {content}
     </main>
   );
 }
--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,7 +1,10 @@
-export default function Button(){
-    return (
-        <button className="px-4 py-2 text-xs md:text-base rounded-md bg-stone-700 text-stone-400 hover:bg-stone-600 hover:text-stone-100">
-        + Add Project
+export default function Button({ onClick, children }) {
+  return (
+    <button
+      onClick={onClick}
+      className="px-4 py-2 bg-stone-500 text-white rounded hover:bg-stone-700"
+    >
+      {children}
     </button>
-    );
+  );
 }
